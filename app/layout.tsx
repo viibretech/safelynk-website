da69d871cd8c2e3515d6d669f61030ec0eb7eb6a
samuel <samueltwumasi1290@gmail.com>
2025-11-04 20:11:32 +0000

    update

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Viibre Technologies",
+  description:
+    "A technology company focused on building innovative digital solutions and providing technical services to individuals and businesses. We specialize in creating secure, user-friendly platforms that solve everyday challenges and enhance digital interactions across various industries.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
